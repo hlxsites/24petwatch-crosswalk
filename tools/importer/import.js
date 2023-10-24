3ff4a2f05ec19967fa1cf38f751b477f6005f15e
Chris Bohnert <38424477+bohnertchris@users.noreply.github.com>
2023-10-24 10:51:57 +0200

    Attempt to generalize column creation

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -143,6 +143,94 @@ const createColumns = (main, document, newMain) => {
 
 };
 
+// Generalize creating columns from grids
+//Extract all aem-Grid--12 that have aem-GridColumn--default--6
+// 12 is the container (100%) that holds two columns of 6 (50% each)
+const createGrids = (main, document, newMain) => {
+
+  // Find all aem-GridColumn--default--6 that are children of aem-Grid--12
+  const cols = main.querySelectorAll('div.aem-Grid--12 > div.aem-GridColumn--default--6');
+  let newCols = [];
+
+  if (cols) {
+    
+    for( let i = 0; i < cols.length; i += 1) {
+      
+      const div = document.createElement('div');
+      
+      // H1 heading
+      if ( cols[i].querySelector('h1') ) {
+        const h1 = document.createElement('h1');
+        h1.textContent = cols[i].querySelector('h1').textContent;
+        div.append(h1);
+      }
+
+      // H2 heading
+      if ( cols[i].querySelector('h2') ) {
+        const h2 = document.createElement('h2');
+        h2.textContent = cols[i].querySelector('h2').textContent;
+        div.append(h2);
+      }
+
+      // H4 heading
+        if ( cols[i].querySelector('h4') ) {
+        const h4 = document.createElement('h4');
+        h4.textContent = cols[i].querySelector('h4').textContent;
+        div.append(h4);
+      }
+
+      // paragraphs
+      if ( cols[i].querySelector('p') ) {
+        const p = document.createElement('p');
+        p.textContent = cols[i].querySelector('p').textContent;
+        div.append(p);
+      }
+
+      // image
+      if ( cols[i].querySelector('img') ) {
+        const img = document.createElement('img');
+        const imgSrc = cols[i].querySelector('img').getAttribute('src');
+        img.setAttribute('src', imgSrc);
+        div.append(img);
+      }
+ 
+      // Check if current and next nodes are siblings and if so, add current node to grid, otherwise output current grid
+      // First see if next element exists and if not print grid as-is
+      if ( i+1 >= cols.length ) {
+        newCols.push(div);
+        // Build array for columns
+        const cells = [
+          ['Columns'],
+          newCols,
+        ];
+
+        const block = WebImporter.DOMUtils.createTable(cells,document);
+        newMain.append(block);
+      }
+      // Check if the next node and current are siblings, if so, just add column to array to add it them to the same grid
+      else if (cols[i].parentNode === cols[i+1].parentNode){
+        newCols.push(div);
+      }
+      // Otherwise the current node and next aren't siblings, so print out array and start a new grid
+      else {
+        newCols.push(div);
+        // Build array for columns
+        const cells = [
+          ['Columns'],
+          newCols,
+        ];
+
+        const block = WebImporter.DOMUtils.createTable(cells,document);
+        newMain.append(block);
+        
+        // Reset columns array to an empty one to start a new grid
+        newCols = [];
+      }
+    }
+  }
+
+};
+
 const createCards = (main, document) => {
   main.querySelectorAll('fulllayout').forEach((fl) => {
     const cards = [];
@@ -441,6 +529,7 @@ export default {
     createFullLayoutSection(main, document);
     createHero(main, document, newMain);
     createColumns(main, document, newMain);
+    createGrids(main, document, newMain);
     createCards(main, document);
     createLogoCloud(main, document);
     createWeSee(main, document);
